37fca4e79f996d232c8b2f40ea14c6c2c62a772a
Thomas Sullivan <thomas.sullivan@protedyne.com>
2021-03-05 16:06:51 -0500

    updated todos and docs.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,17 @@ fn main() {
     let mut thingy = signal_device::new(format!("thing"));
     println!("Using device address of: {}", thingy.get_device());
 
+    // For each of the signals in the yaml hash under "signals:", it should make a signal object.
+    // It is then up to the device code (here) to call thingy.get_signal("signalName").status()
+    // to get the current status of the signal.
+    //
+    // Phase 2 will have each of those signals: hash values constantly updated with a scanner
+    // thread so we can call get_signal("signalName").status() and it'll return the cached copy
+    // (like what happens in the java side now)
+    //
+    // thingy.get_signal(String) should return a reference to the signal struct from the hashmap of
+    // all created signals.
+
     loop {
         println!("Signal value is: {}", thingy.get_signal());
         std::thread::sleep(time::Duration::from_millis(1000));
