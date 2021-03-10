b3816cc802a944b85eb219d159830fc6591fa427
Thomas Sullivan <thomas.sullivan@protedyne.com>
2021-03-10 13:24:55 -0500

    added use of servo device and some general updates and flow changes.

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,25 @@ use signal_device;
 use std::time;
 
 fn main() {
-    // This brick is a modbus device, with I/O.  Lets just use a single one for now,
-    // in the future we will pass to new() the name of the device config file for it
-    // to load device detail from;
-    let mut thingy = signal_device::new(format!("thing"));
-    println!("Using device address of: {}", thingy.get_device());
+    // This is the applied servo that is controllable via modbus tcp
+    let mut servo_lifter = applied_device::new(format!("thing"), format!("servo_lifter"));
+    println!("{}", servo_lifter.to_string());
+    println!(
+        "Current encoder position: {}",
+        servo_lifter.get_encoder_count()
+    );
 
-    // For each of the signals in the yaml hash under "signals:", it should make a signal object.
-    // It is then up to the device code (here) to call thingy.get_signal("signalName").status()
-    // to get the current status of the signal.
-    //
-    // Phase 2 will have each of those signals: hash values constantly updated with a scanner
-    // thread so we can call get_signal("signalName").status() and it'll return the cached copy
-    // (like what happens in the java side now)
-    //
-    // thingy.get_signal(String) should return a reference to the signal struct from the hashmap of
-    // all created signals.
+    // This brick is a modbus device, with I/O.
+    // // This will setup all available signals
+    let mut signal_device = signal_device::new(format!("thing"));
+    println!("{}", signal_device.to_string());
 
-    let mut applied_servo = applied_device::new();
-
+    // The main application/device loop
     loop {
         println!(
             "Signal value is: {}",
             //thingy.get_signal().get_signal_status()
-            thingy.get_signal_directly()
+            signal_device.get_signal_directly() // TODO: pass to it a name of the signal
         );
         std::thread::sleep(time::Duration::from_millis(500));
     }
