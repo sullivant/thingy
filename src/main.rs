9d6da825c15e806101f6dee47be3b1f607d2e423
Thomas Sullivan <thomas.sullivan@protedyne.com>
2021-03-11 16:34:29 -0500

    got a better cycle loop working with some debug info as well.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,19 @@ use applied_device;
 use signal_device;
 use std::time;
 
+static RETRACT_POS: u64 = 20000;
+static EXTEND_POS: u64 = 300000;
+
 fn main() {
     // This is the applied servo that is controllable via modbus tcp
     let mut servo_lifter = applied_device::new(format!("thing"), format!("servo_lifter"));
     println!("{}", servo_lifter.to_string());
-    println!(
-        "Current encoder position: {}",
-        servo_lifter.get_encoder_count()
-    );
+
+    println!("Servo status: {:?}", servo_lifter.get_servo_status());
+    println!("Servo alarms: {:?}", servo_lifter.get_servo_alarms());
+
+    servo_lifter.home_servo();
+    servo_lifter.move_servo(400, 400, 1500, RETRACT_POS);
 
     // This brick is a modbus device, with I/O.
     // // This will setup all available signals
@@ -18,11 +23,11 @@ fn main() {
 
     // The main application/device loop
     loop {
-        println!(
-            "Signal value is: {}",
-            //thingy.get_signal().get_signal_status()
-            signal_device.get_signal_directly() // TODO: pass to it a name of the signal
-        );
-        std::thread::sleep(time::Duration::from_millis(500));
+        if signal_device.get_signal_directly() {
+            servo_lifter.move_servo(400, 400, 1500, EXTEND_POS);
+        } else {
+            servo_lifter.move_servo(400, 400, 1500, RETRACT_POS);
+        }
+        std::thread::sleep(time::Duration::from_millis(5));
     }
 }
