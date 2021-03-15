0c1351c9b5a3d0bda71ad403ef477a7a7a756854
Thomas Sullivan <thomas.sullivan@protedyne.com>
2021-03-15 15:38:03 -0400

    Added usage of log4rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use applied_device;
+use log::{error, info, warn};
+use log4rs;
 use signal_device;
 use std::time;
 
@@ -6,27 +8,30 @@ static RETRACT_POS: u64 = 20000;
 static EXTEND_POS: u64 = 300000;
 
 fn main() {
+    log4rs::init_file("thingy/resources/log4rs.yaml", Default::default()).unwrap();
+
     // This is the applied servo that is controllable via modbus tcp
     let mut servo_lifter = match applied_device::new(format!("thing"), format!("servo_lifter")) {
         Ok(s) => s,
         Err(e) => {
-            println!("Unable to create applied device, cannot continue: {}", e);
-            return;
-        }
-    };
-    // This is the B&R brick
-    let mut signal_device = match signal_device::new(format!("thing")) {
-        Ok(s) => s,
-        Err(e) => {
-            println!("Unable to create signal device, cannot continue: {}", e);
+            error!("Unable to create applied device, cannot continue: {}", e);
             return;
         }
     };
 
-    println!("{}", servo_lifter.to_string());
-    println!("Servo status: {:?}", servo_lifter.get_servo_status());
-    println!("Servo alarms: {:?}", servo_lifter.get_servo_alarms());
-    println!("{}", signal_device.to_string());
+    // This is the B&R brick
+    let mut signal_device = match signal_device::new(format!("thing")) {
+        Ok(s) => s,
+        Err(e) => {
+            error!("Unable to create signal device, cannot continue: {}", e);
+            return;
+        }
+    };
+
+    info!("{}", servo_lifter.to_string());
+    info!("Servo status: {:?}", servo_lifter.get_servo_status());
+    info!("Servo alarms: {:?}", servo_lifter.get_servo_alarms());
+    info!("{}", signal_device.to_string());
 
     // Start the actual device features needed
     servo_lifter.home_servo();
@@ -40,7 +45,7 @@ fn main() {
             Ok(true) => servo_lifter.move_servo(400, 400, 1500, EXTEND_POS),
             Ok(false) => servo_lifter.move_servo(400, 400, 1500, RETRACT_POS),
             Err(e) => {
-                println!("{}", e)
+                warn!("{}", e)
             }
         };
 
