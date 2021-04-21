e3905fee5043bf835072e0d40ed100b43154baaa
Thomas Sullivan <thomas.sullivan@protedyne.com>
2021-04-21 08:23:49 -0400

    Trying to work with a scanner wrapper for signals.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use applied_device;
-use log::{error, info, warn};
+use log::{error, info};
 use log4rs;
-use signal_device;
+use signal_device::SignalDevice;
+use signal_scanner::SignalScanner;
 use std::time;
 
 static RETRACT_POS: u64 = 20000;
@@ -9,6 +10,22 @@ static EXTEND_POS: u64 = 300000;
 
 fn main() {
     log4rs::init_file("thingy/resources/log4rs.yaml", Default::default()).unwrap();
+
+    // Create a modbus signal scanner and pass to it the B&R brick to scan
+    let mut signal_scanner = SignalScanner::new(format!("scanman!"));
+
+    // This is the B&R brick
+    let coupler = signal_device::new(format!("thing")).expect("Cannot continue:");
+
+    &signal_scanner.register_device(format!("brick"), coupler);
+    println!("Signal Scanner name: {}", signal_scanner.get_thread_text());
+
+    let brick: &mut SignalDevice = signal_scanner
+        .get_device_mut(&"brick")
+        .expect("Unable to find signal device: Brick");
+
+    println!("Brick: {:?}", brick);
+    brick.set_coupler_address(format!("1.1.1.1"));
 
     // This is the applied servo that is controllable via modbus tcp
     let mut servo_lifter = match applied_device::new(format!("thing"), format!("servo_lifter")) {
@@ -19,19 +36,9 @@ fn main() {
         }
     };
 
-    // This is the B&R brick
-    let mut signal_device = match signal_device::new(format!("thing")) {
-        Ok(s) => s,
-        Err(e) => {
-            error!("Unable to create signal device, cannot continue: {}", e);
-            return;
-        }
-    };
-
     info!("{}", servo_lifter.to_string());
     info!("Servo status: {:?}", servo_lifter.get_servo_status());
     info!("Servo alarms: {:?}", servo_lifter.get_servo_alarms());
-    info!("{}", signal_device.to_string());
 
     // Start the actual device features needed
     servo_lifter.home_servo();
@@ -39,16 +46,20 @@ fn main() {
 
     let signal_name: String = "diInputSensor".to_string();
 
-    // The main application/device loop
+    // Loop until we get the signal once.
     loop {
-        match signal_device.get_signal_directly(&signal_name) {
-            Ok(true) => servo_lifter.move_servo(400, 400, 1500, EXTEND_POS),
-            Ok(false) => servo_lifter.move_servo(400, 400, 1500, RETRACT_POS),
-            Err(e) => {
-                warn!("{}", e)
-            }
+        if brick.get_signal_directly(&signal_name).unwrap() {
+            break;
         };
-
         std::thread::sleep(time::Duration::from_millis(5));
     }
+
+    info!("Got signal to extend and then retract.");
+    servo_lifter.move_servo(400, 400, 1500, EXTEND_POS);
+    std::thread::sleep(time::Duration::from_millis(1000));
+    servo_lifter.move_servo(400, 400, 1500, RETRACT_POS);
+
+    // Lets send that disconnect command, see what happens...
+    servo_lifter.shutdown();
+    info!("Servo status: {:?}", servo_lifter.get_servo_status());
 }
